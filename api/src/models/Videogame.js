10a0eac43bf8db8e3c0026bc4b5395476324ee3d
rodriguezviktor92 <rodriguezviktor@gmail.com>
2022-03-16 15:45:32 -0500

    Videogame model add image and id fields

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,6 +4,11 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define("videogame", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -16,6 +21,9 @@ module.exports = (sequelize) => {
       type: DataTypes.DATE,
     },
     rating: {
+      type: DataTypes.STRING,
+    },
+    background_image: {
       type: DataTypes.STRING,
     },
   });
