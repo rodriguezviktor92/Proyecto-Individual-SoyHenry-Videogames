634a1a434f8be6f4233ed8ae3904999b92b013bc
rodriguezviktor92 <rodriguezviktor@gmail.com>
2022-03-16 15:48:42 -0500

    GET genres route

--- api/src/routes/controllers/genres.js
+++ api/src/routes/controllers/genres.js
@@ -1,5 +1,26 @@
+require("dotenv").config();
+const { API } = process.env;
+const axios = require("axios");
+const { Genre } = require("../../db.js");
+
 module.exports = {
-  get(req, res) {
-    res.send("GET genre");
+  async get(req, res) {
+    try {
+      const genresDB = await Genre.findAll();
+
+      if (genresDB.length === 0) {
+        const genresAPI = await axios.get(
+          `https://api.rawg.io/api/genres?key=${API}`
+        );
+        const results = genresAPI.data.results.map((e) => {
+          return { id: e.id, name: e.name };
+        });
+        Genre.bulkCreate(results).then((result) => res.send(result));
+      } else {
+        res.send(genresDB);
+      }
+    } catch (err) {
+      res.status(500).send(err);
+    }
   },
 };
