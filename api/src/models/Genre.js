1707accd0f59d62280955194b3f22c830ee439fb
rodriguezviktor92 <rodriguezviktor@gmail.com>
2022-03-16 15:24:50 -0500

    Gener model add id field

--- api/src/models/Genre.js
+++ api/src/models/Genre.js
@@ -2,8 +2,15 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
   sequelize.define("genre", {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
   });
 };
