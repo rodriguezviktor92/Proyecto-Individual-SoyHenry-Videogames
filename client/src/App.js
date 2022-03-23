2b5afb59fec9846efbe68d37fb4b028c614aca98
rodriguezviktor92 <rodriguezviktor@gmail.com>
2022-03-23 15:29:43 -0500

    add route /, route /home, useDispacth and useEffect to get all videogames

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,35 @@
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav/Nav";
+import Landing from "./components/Landing/Landing";
+import LeftBar from "./components/LeftBar/LeftBar";
+import GameContent from "./components/GameContent/GameContent";
+import "./App.css";
+import { useDispatch } from "react-redux";
+import { getAllVideogames } from "./redux/actions";
 
 function App() {
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    dispatch(getAllVideogames());
+  }, []);
+
   return (
     <div className="App">
-      <h1>Henry Videogames</h1>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route
+          path="/home"
+          element={
+            <>
+              <Nav />
+              <LeftBar />
+              <GameContent />
+            </>
+          }
+        />
+      </Routes>
     </div>
   );
 }
