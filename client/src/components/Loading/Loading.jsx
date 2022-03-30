5f8e3606d941c26696e05036ee4f69370134bbb5
rodriguezviktor92 <rodriguezviktor@gmail.com>
2022-03-30 17:31:24 -0500

    Loading component creation and styling

---
+++ client/src/components/Loading/Loading.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import css from "./Loading.module.css";
+
+export default function Loading() {
+  return (
+    <div className={css.center}>
+      <img src="https://i.ibb.co/tYV1v2c/loading.png" alt="" />
+    </div>
+  );
+}
