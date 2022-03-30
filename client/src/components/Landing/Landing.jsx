5c21c15081aa5d53c7c95cf524c0423e6c8fa0ec
rodriguezviktor92 <rodriguezviktor@gmail.com>
2022-03-30 17:23:45 -0500

    add style to Landing component

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import css from "./Landing.module.css";
 
 function Landing() {
   return (
-    <div>
-      <h1>Landing</h1>
-      <Link to="/home">Home</Link>
+    <div className={css.content}>
+      <div>
+        <img src="https://i.ibb.co/fFKyX4X/image-1.png" alt="" />
+      </div>
+      <div>
+        <h1>The Best Video Game Discovery WebSite</h1>
+        <Link to="/home" className={css.button}>
+          Enter Now
+        </Link>
+      </div>
     </div>
   );
 }
